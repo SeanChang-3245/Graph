cb4ff2051dd67e477d18d699cd7a424ab0ebcfdf
SeanChang3245 <seanchang3245@gmail.com>
2020-12-11 22:47:30 +0800

    123

--- Graph/Solution.h
+++ Graph/Solution.h
@@ -8,6 +8,7 @@ struct Node;
 
 struct Node
 {
+    bool used = false;
     int money;
     vector<edge> path;
 };
@@ -47,7 +48,8 @@ class graph
 
         for (edge i : current_node->path)
         {
-            brute(remain_time - i.cost, current_money + i.target->money, i.target);
+            if(i.target->used == false)
+                brute(remain_time - i.cost, current_money + i.target->money, i.target);
         }
     }
 
@@ -56,19 +58,94 @@ class graph
     {
         Node* current_node = this->root;
         int current_money = 0;
+        edge tmp;
+
+        tmp.cost = INT_MAX;
+
 
         while (remain_time >= 0)
         {
             for (edge i : current_node->path)
             {
-
-
+                if (i.cost < tmp.cost && i.target->used == false)
+                    tmp = i;
             }
 
+            if (remain_time - tmp.cost >= 0)
+            {
+                remain_time -= tmp.cost;
+                current_money += tmp.target->money;
+            }
+            else
+            {
+                break;
+            }
         }
-
+        cout << current_money;
     }
 
+
+    void greedy_money(int remain_time)
+    {
+        Node* current_node = this->root;
+        int current_money = 0;
+        edge tmp;
+
+        tmp.target->money = INT_MIN;
+
+
+        while (remain_time >= 0)
+        {
+            for (edge i : current_node->path)
+            {
+                if (i.target->money > tmp.target->money && i.target->used == false)
+                    tmp = i;
+            }
+
+            if (remain_time - tmp.cost >= 0)
+            {
+                remain_time -= tmp.cost;
+                current_money += tmp.target->money;
+            }
+            else
+            {
+                break;
+            }
+        }
+        cout << current_money;
+    }
+
+
+    void greedy_cp(int remain_time)
+    {
+        Node* current_node = this->root;
+        int current_money = 0;
+        edge tmp;
+
+        tmp.target->money = 0;
+        tmp.cost = 1;
+
+
+        while (remain_time >= 0)
+        {
+            for (edge i : current_node->path)
+            {
+                if ((double)i.target->money / i.cost > (double)tmp.target->money / tmp.cost && i.target->used == false)
+                    tmp = i;
+            }
+
+            if (remain_time - tmp.cost >= 0)
+            {
+                remain_time -= tmp.cost;
+                current_money += tmp.target->money;
+            }
+            else
+            {
+                break;
+            }
+        }
+        cout << current_money;
+    }
 
 
 
